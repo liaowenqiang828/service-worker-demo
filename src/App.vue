17ea3d39e342a5cc73ac3b97e49cd83245c6d8fb
liaowenqiang828 <18840965351@163.com>
2022-01-10 17:19:21 +0800

    add service worker logic

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,72 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <button
+      v-if="updateExists"
+      @click="refreshApp"
+      class="btn"
+    >New version available! Click to update</button>
+    <div v-else>There's no update yet.</div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  name: "app",
+  data() {
+    return {
+      refreshing: false,
+      worker: null,
+      updateExists: false
+    };
+  },
+
+  created() {
+    // Listen for swUpdated event and display refresh snackbar as required.
+    document.addEventListener("swUpdated", this.showRefreshUI, { once: true });
+    // Refresh all open app tabs when a new service worker is installed.
+    navigator.serviceWorker &&
+      navigator.serviceWorker.addEventListener(
+        //triggered by registration.claim
+        "controllerchange",
+        () => {
+          if (this.refreshing) return;
+          this.refreshing = true;
+          console.log("controllerchange triggered, -> auto refresh!!");
+          window.location.reload();
+        }
+      );
+  },
+
+  methods: {
+    showRefreshUI(e) {
+      // Display a button inviting the user to refresh/reload the app due
+      // to an app update being available.
+      // The new service worker is installed, but not yet active.
+      // Store the ServiceWorkerRegistration instance for later use.
+      if (e.detail) {
+        this.worker = e.detail;
+        this.updateExists = true;
+        console.log("Showing refresh button.");
+        console.log(this.worker);
+      } else {
+        console.warn("No worker data found!");
+      }
+    },
+    refreshApp() {
+      console.log("skipWaiting started");
+      console.log(this.worker);
+      // Handle a user tap on the update app button.
+      this.updateExists = false;
+      // Protect against missing registration.waiting.
+      if (!this.worker) {
+        console.warn("No worker data found when trying to refresh!");
+        return;
+      }
+      this.worker.postMessage({ type: "SKIP_WAITING" });
+      console.log("skipWaiting finished");
+    }
   }
-}
+};
 </script>
 
 <style>
