591781a7dde45af9759c605effd228971220c6c6
liaowenqiang828 <18840965351@163.com>
2022-01-19 23:52:47 +0800

    [Wenqaing] put service work file out of assets fold

--- src/registerServiceWorker.js
+++ src/registerServiceWorker.js
@@ -11,11 +11,16 @@ if ('serviceWorker' in navigator) {
   console.log("${process.env.BASE_URL}", `${process.env.BASE_URL}`);
 
   window.addEventListener('load', () => {
-    navigator.serviceWorker.register('assets/service-worker.js').then(registration => {
-      console.log("service worker register successful", registration.scope);
-    }, error => {
-      console.log('service worker register failed', error)
-    })
+    navigator.serviceWorker
+      .register("https://dreamy-hamilton-33d7dc.netlify.app/service-worker.js")
+      .then(
+        (registration) => {
+          console.log("service worker register successful", registration.scope);
+        },
+        (error) => {
+          console.log("service worker register failed", error);
+        }
+      );
   })
 }
 
