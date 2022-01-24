0c0c97749b7b4963af640c80130e8d7bbe7d1211
liaowenqiang828 <18840965351@163.com>
2022-01-24 23:19:26 +0800

    [Wenqiang] correct service-worker content

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,19 +1,27 @@
-self.addEventListener('install', event => {
-  console.log("service worker installing...")
+workbox.core.setCacheNameDetails({
+  prefix: "service-worker-demo-pwa",
+  suffix: "v1.0.0",
+});
 
-  event.waitUntil(
-    caches.open("cacheName").then(cache => {
-      return cache.addAll()
-    })
-  )
-})
+workbox.precaching.precacheAndRoute(self.__precacheManifest || []);
 
-self.addEventListener('fetch', event => {
-  console.log("event.request.url", event.request.url);
 
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request)
-    })
-  )
-})
+// self.addEventListener('install', event => {
+//   console.log("service worker installing...")
+
+//   event.waitUntil(
+//     caches.open("cacheName").then(cache => {
+//       return cache.addAll()
+//     })
+//   )
+// })
+
+// self.addEventListener('fetch', event => {
+//   console.log("event.request.url", event.request.url);
+
+//   event.respondWith(
+//     caches.match(event.request).then(response => {
+//       return response || fetch(event.request)
+//     })
+//   )
+// })
