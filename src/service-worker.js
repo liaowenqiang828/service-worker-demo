927841fc726a8caeff1b657967cf094b27c30347
liaowenqiang828 <18840965351@163.com>
2022-03-24 17:02:25 +0800

    [Wenqiang ]Add log into service worker

--- src/service-worker.js
+++ src/service-worker.js
@@ -13,8 +13,13 @@ self.addEventListener('install', function(event) {
 self.addEventListener('fetch', function (event) {
     event.respondWith( async function() {
         try {
+            console.log("event.request to network", event.request);
+            console.log("result from network", await fetch(event.request));
             return await fetch(event.request);
         } catch {
+            console.log("event.request to caches", event.request);
+            console.log("result from caches", caches.match(event.request));
+            
             return caches.match(event.request)
         }
     }())
