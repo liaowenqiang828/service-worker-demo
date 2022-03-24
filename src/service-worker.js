5fea2dc42487b3078cef2fa60d8cbf5565579957
liaowenqiang828 <18840965351@163.com>
2022-03-24 20:01:45 +0800

    [Wenqiang] add url

--- src/service-worker.js
+++ src/service-worker.js
@@ -11,28 +11,13 @@ self.addEventListener('install', function(event) {
     self.skipWaiting()
 })
 
-// self.addEventListener('fetch', function (event) {
-//     event.respondWith( async function() {
-//         try {
-//             console.log("event.request to network", event.request);
-//             console.log("result from network", await fetch(event.request));
-//             return await fetch(event.request);
-//         } catch {
-//             console.log("event.request to caches", event.request);
-//             console.log("result from caches", caches.match(event.request));
-            
-//             return caches.match(event.request)
-//         }
-//     }())
-// })
-
 self.addEventListener("fetch", (event) => {
   event.respondWith(
     (async function () {
       try {
         console.log("event.request to network", event.request);
         console.log("result from network", await fetch(event.request));
-        return await fetch(event.request);
+        return await fetch(event.request.url);
       } catch (err) {
         console.log("event.request to caches", event.request);
         console.log("result from caches", caches.match(event.request));
@@ -41,3 +26,17 @@ self.addEventListener("fetch", (event) => {
     })()
   );
 });
+
+// self.addEventListener('fetch', function (e) {
+//     e.respondWith(
+//         caches.match(e.request).then(function (response) {
+//             if (response != null) {
+//                 console.log(`fetch:${e.request.url} from cache`);
+//                 return response
+//             } else {
+//                 console.log(`fetch:${e.request.url} from http`);
+//                 return fetch(e.request.url)
+//             }
+//         })
+//     )
+// })
