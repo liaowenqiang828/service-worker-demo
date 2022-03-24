6aa5e5e2a364150ccec412fb1bb1d5278ea5e532
liaowenqiang828 <18840965351@163.com>
2022-03-24 17:09:13 +0800

    [Wenqiang] fix network first stratege

--- src/service-worker.js
+++ src/service-worker.js
@@ -10,17 +10,29 @@ self.addEventListener('install', function(event) {
     self.skipWaiting()
 })
 
-self.addEventListener('fetch', function (event) {
-    event.respondWith( async function() {
-        try {
-            console.log("event.request to network", event.request);
-            console.log("result from network", await fetch(event.request));
-            return await fetch(event.request);
-        } catch {
-            console.log("event.request to caches", event.request);
-            console.log("result from caches", caches.match(event.request));
+// self.addEventListener('fetch', function (event) {
+//     event.respondWith( async function() {
+//         try {
+//             console.log("event.request to network", event.request);
+//             console.log("result from network", await fetch(event.request));
+//             return await fetch(event.request);
+//         } catch {
+//             console.log("event.request to caches", event.request);
+//             console.log("result from caches", caches.match(event.request));
             
-            return caches.match(event.request)
-        }
-    }())
-})
+//             return caches.match(event.request)
+//         }
+//     }())
+// })
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    (async function () {
+      try {
+        return await fetch(event.request);
+      } catch (err) {
+        return caches.match(event.request);
+      }
+    })()
+  );
+});
