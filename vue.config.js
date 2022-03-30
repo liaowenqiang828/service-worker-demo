7d165fc74fa03883256c5471703035f8af37fe32
liaowenqiang828 <18840965351@163.com>
2022-03-30 16:40:12 +0800

    [Wenqiang] change service-work file position

--- vue.config.js
+++ vue.config.js
@@ -9,7 +9,7 @@ module.exports = {
     appleMobileWebAppStatusBarStyle: "black",
     workboxPluginMode: "InjectManifest",
     workboxOptions: {
-      swSrc: "src/service-worker.js",
+      swSrc: "./service-worker.js",
     },
   },
   pluginOptions: {
