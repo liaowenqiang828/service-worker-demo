5998a021fd60150fac813513a27267fa44fb976e
liaowenqiang828 <18840965351@163.com>
2022-03-30 16:58:57 +0800

    [Wenqiang] add a button and change css

--- src/App.vue
+++ src/App.vue
@@ -1,52 +1,55 @@
 <template>
   <div id="app">
-  <div class="content">
-    <h3>Button Components</h3>
-    <div class="button-row">
-      <MyButton /> 
-      <MyButton /> 
+    <div class="content">
+      <h3>Button Components</h3>
+      <div class="button-row">
+        <MyButton />
+        <MyButton />
+      </div>
+      <div class="button-row">
+        <MyButton variant="outline" />
+        <MyButton variant="outline" />
+      </div>
+      <div class="button-row">
+        <MyButton variant="text" />
+        <MyButton variant="text" />
+      </div>
+      <div class="button-row">
+        <MyButton disableShadow />
+      </div>
+      <div class="button-row">
+        <MyButton disabled />
+        <MyButton variant="text" disabled />
+      </div>
+      <div class="button-row">
+        <MyButton size="sm" />
+        <MyButton size="md" />
+        <MyButton size="lg" />
+      </div>
+      <div class="button-row">
+        <MyButton color="default" />
+      </div>
+      <div class="button-row">
+        <MyButton color="primary" />
+      </div>
+      <div class="button-row">
+        <MyButton color="secondary" />
+      </div>
+      <div class="button-row">
+        <MyButton color="danger" name="version three" />
+      </div>
+      <div class="button-row">
+        <MyButton color="danger" name="version four" />
+      </div>
+      <div class="button-row">
+        <MyButton color="danger" name="version five" />
+      </div>
     </div>
-    <div class="button-row">
-      <MyButton variant="outline"/>
-      <MyButton variant="outline"/>
-    </div>
-    <div class="button-row">
-      <MyButton variant="text"/>
-      <MyButton variant="text"/>
-    </div>
-    <div class="button-row">
-      <MyButton disableShadow/>
-    </div>
-    <div class="button-row">
-      <MyButton disabled/>
-      <MyButton variant="text" disabled/>
-    </div>
-    <div class="button-row">
-      <MyButton size="sm"/>
-      <MyButton size="md"/>
-      <MyButton size="lg"/>
-    </div>
-    <div class="button-row">
-      <MyButton color="default"/>
-    </div>
-    <div class="button-row">
-      <MyButton color="primary"/>
-    </div>
-    <div class="button-row">
-      <MyButton color="secondary"/>
-    </div>
-    <div class="button-row">
-      <MyButton color="danger" name="version three"/>
-    </div>
-    <div class="button-row">
-      <MyButton color="danger" name="version four"/>
-    </div>
-  </div>
   </div>
 </template>
 
 <script>
-import MyButton from './components/MyButton.vue'
+import MyButton from "./components/MyButton.vue";
 
 export default {
   name: "app",
@@ -57,13 +60,12 @@ export default {
     return {
       refreshing: false,
       worker: null,
-      updateExists: false
+      updateExists: false,
     };
   },
 
   created() {},
-  methods: {
-  }
+  methods: {},
 };
 </script>
 
