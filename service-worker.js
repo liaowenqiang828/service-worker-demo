3498a1ddc5b2f61d0a15e4e708bcfc9b10aa5186
liaowenqiang828 <18840965351@163.com>
2022-03-31 10:46:21 +0800

    [Wenqiang] add a button and change css

--- service-worker.js
+++ service-worker.js
@@ -11,22 +11,26 @@ self.addEventListener("install", function (event) {
 });
 
 self.addEventListener("fetch", (event) => {
-  event.respondWith(
-    (async function () {
-      try {
-        console.log("event.request to network", event.request);
-        console.log("result from network", await fetch(event.request));
-        console.log("event request destination", event.request.destination);
-        console.log("event request url", event.request.url);
-        console.log(event.request.url.endsWith(".js"));
-        return await fetch(event.request);
-      } catch (err) {
-        console.log("event.request to caches", event.request);
-        console.log("result from caches", caches.match(event.request));
-        return caches.match(event.request);
-      }
-    })()
-  );
+  if (event.request.url.endsWith("css")) {
+    return event.respondWith(fetch(enevt.request));
+  }
+
+  // event.respondWith(
+  //   (async function () {
+  //     try {
+  //       console.log("event.request to network", event.request);
+  //       console.log("result from network", await fetch(event.request));
+  //       console.log("event request destination", event.request.destination);
+  //       console.log("event request url", event.request.url);
+  //       console.log(event.request.url.endsWith(".js"));
+  //       return await fetch(event.request);
+  //     } catch (err) {
+  //       console.log("event.request to caches", event.request);
+  //       console.log("result from caches", caches.match(event.request));
+  //       return caches.match(event.request);
+  //     }
+  //   })()
+  // );
 });
 
 // self.addEventListener("script", (event) => {
