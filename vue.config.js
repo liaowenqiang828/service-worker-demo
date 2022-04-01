8b3989d554d21c7881a0fbddf06302c822affeb4
liaowenqiang828 <18840965351@163.com>
2022-04-01 16:58:24 +0800

    [Wenqiang] use update

--- vue.config.js
+++ vue.config.js
@@ -13,38 +13,6 @@ module.exports = {
     },
   },
 
-  // // Other webpack config...
-  // plugins: [
-  //   // Other plugins...
-  //   new GenerateSW({
-  //     // These are some common options, and not all are required.
-  //     // Consult the docs for more info.
-  //     exclude: [/.../, '...'],
-  //     maximumFileSizeToCacheInBytes: ...,
-  //     navigateFallback: '...',
-  //     runtimeCaching: [{
-  //       // Routing via a matchCallback function:
-  //       urlPattern: ({request, url}) => ...,
-  //       handler: '...',
-  //       options: {
-  //         cacheName: '...',
-  //         expiration: {
-  //           maxEntries: ...,
-  //         },
-  //       },
-  //     }, {
-  //       // Routing via a RegExp:
-  //       urlPattern: new RegExp('...'),
-  //       handler: '...',
-  //       options: {
-  //         cacheName: '...',
-  //         plugins: [..., ...],
-  //       },
-  //     }],
-  //     skipWaiting: ...,
-  //   }),
-  // ],
-
   pluginOptions: {
     "style-resources-loader": {
       preProcessor: "less",
